b6d2482a42efe2386f23df91473d59e5f5b0d14e
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-08-08 05:01:39 +0900

    Update auth

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,12 +1,20 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import jwt from "jsonwebtoken";
 
 interface AuthType {
   accessToken?: string;
   refreshToken?: string;
+  data?: {
+    _id: string;
+  };
 }
 const initialState: AuthType = {
   accessToken: localStorage.getItem("accessToken") || undefined,
   refreshToken: localStorage.getItem("refreshToken") || undefined,
+  data:
+    (localStorage.getItem("accessToken") &&
+      (jwt.decode(localStorage.getItem("accessToken") as string) as any)) ||
+    undefined,
 };
 
 const authSlice = createSlice({
@@ -20,6 +28,7 @@ const authSlice = createSlice({
       const { accessToken, refreshToken } = action.payload;
       state.accessToken = accessToken;
       state.refreshToken = refreshToken;
+      state.data = jwt.decode(accessToken) as any;
     },
   },
 });
