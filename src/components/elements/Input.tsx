9057a71153fb67d381ad968c2bd0e0601a3e7e34
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-08-07 17:47:37 +0900

    Update Input Element

---
+++ src/components/elements/Input.tsx
@@ -0,0 +1,32 @@
+import styled, { css } from "styled-components";
+
+interface InputProps {
+  shadow?: boolean | string;
+  fullWidth?: boolean;
+  block?: boolean;
+}
+const Input = styled.input<InputProps>`
+  border: 0;
+  padding: 1em 1.5em;
+  background: #f2f2f2;
+  box-sizing: border-box;
+  border-radius: 1em;
+  ${({ shadow }) =>
+    shadow &&
+    css`
+      box-shadow: 2px 2px 5px -1px ${typeof shadow === "string" ? shadow : "#f2f2f2"};
+    `}
+    ${({ fullWidth, block }) =>
+      (fullWidth || block) &&
+      css`
+        display: block;
+      `}
+    ${({ fullWidth }) => css`
+      width: 100%;
+    `}
+  :focus {
+    outline: 0;
+  }
+`;
+
+export default Input;
