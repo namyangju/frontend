b7ca81786556b3264745c3947e5b71dc79e61d12
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-08-07 18:40:00 +0900

    Update IndexPage

--- src/router/IndexPage.tsx
+++ src/router/IndexPage.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import styled from "styled-components";
+import LogIn from "components/auth/LogIn";
 
 function IndexPage() {
-  return <div>메인 페이지</div>;
+  return (
+    <Wrap>
+      <div className="logo">로고 들어갈 부분</div>
+      <div className="login-wrap">
+        <LogIn />
+      </div>
+    </Wrap>
+  );
 }
+
+const Wrap = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  .logo {
+    width: 300px;
+    height: 70px;
+    background: gray;
+  }
+  .login-wrap {
+    width: 500px;
+  }
+`;
 
 export default IndexPage;
