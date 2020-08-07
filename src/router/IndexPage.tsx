29d8445cff3c827ce144c96fe4ba2d862130c34d
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-08-07 22:43:15 +0900

    Update IndexPage

---
+++ src/router/IndexPage.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { css } from "styled-components";
+import { Link } from "react-router-dom";
+import Page from "components/elements/Page";
+import logo from "assets/logo.png";
+
+function IndexPage() {
+  return (
+    <Page css={style}>
+      <header className="header">
+        <div className="left">
+          <div className="logo" role="img" aria-label="logo" />
+        </div>
+        <div className="right">
+          <Link to="/login">로그인</Link>
+          <Link to="/signup">회원가입</Link>
+        </div>
+      </header>
+    </Page>
+  );
+}
+
+const style = css`
+  padding-top: 24px;
+
+  .header {
+    display: flex;
+    align-items: center;
+    .logo {
+      display: block;
+      background: url(${logo}) no-repeat;
+      background-size: 100%;
+      background-position: center;
+      width: 200px;
+      height: 60px;
+    }
+    .right {
+      flex: 1;
+      display: flex;
+      justify-content: flex-end;
+      * {
+        margin-right: 1em;
+        color: black;
+      }
+    }
+  }
+`;
+
+export default IndexPage;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import SignUp from "./SignUpPage";
 import LoginPage from "./LoginPage";
+import IndexPage from "./IndexPage";
 
 function Routes() {
   return (
     <Switch>
+      <Route path="/" component={IndexPage} exact />
       <Route path="/login" component={LoginPage} exact />
       <Route path="/signup" component={SignUp} exact />
     </Switch>
