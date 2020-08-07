2bc58af3c159e5de0f99dfa66542b6ebc57cbced
kimdoyeong <kimdoyeong@users.noreply.github.com>
2020-08-07 13:57:20 +0000

    Automated deployment: Fri Aug  7 13:57:20 UTC 2020 39370ebacb9568cdd096b23580dc9397b2009a2a

---
+++ precache-manifest.f9a80144fdbdfe07f5e469ca15582b3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e41333039bde8b3101e8ec8adca93b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09721bcf95373f92b501",
+    "url": "/static/css/main.1c8de883.chunk.css"
+  },
+  {
+    "revision": "b5af2bc0fb69eab8afbf",
+    "url": "/static/js/2.a6e58928.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.a6e58928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09721bcf95373f92b501",
+    "url": "/static/js/main.2263b6c1.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "40741e130076c40030745dd68f6bd3cb",
+    "url": "/static/media/logo.40741e13.png"
+  }
+]);
