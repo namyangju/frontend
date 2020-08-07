e94442b5b04b4a4a64294ef285a83f47e6460889
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-08-07 19:13:55 +0900

    Update SignUp page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import IndexPage from "./IndexPage";
+import SignUp from "./SignUp";
 
 function Routes() {
   return (
     <Switch>
       <Route path="/" component={IndexPage} exact />
+      <Route path="/signup" component={SignUp} exact />
     </Switch>
   );
 }
